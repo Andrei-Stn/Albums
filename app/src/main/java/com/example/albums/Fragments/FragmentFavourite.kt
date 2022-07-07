c412ca52d07c2a3a139be57357fb989dceed8543
Andrei Stan <andystan291@yahoo.com>
2022-07-07 18:24:27 +0300

    Cleaning Code and added string and dimens resource

--- app/src/main/java/com/example/albums/Fragments/FragmentFavourite.kt
+++ app/src/main/java/com/example/albums/Fragments/FragmentFavourite.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.albums.databinding.FragmentSettingsBinding
+import com.example.albums.databinding.FragmentFavouriteBinding
 
-class FragmentSettings: Fragment() {
-    private var _binding: FragmentSettingsBinding? = null
+class FragmentFavourite: Fragment() {
+    private var _binding: FragmentFavouriteBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -16,9 +16,7 @@ class FragmentSettings: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-
 }
--- app/src/main/java/com/example/albums/MainActivity.kt
+++ app/src/main/java/com/example/albums/MainActivity.kt
@@ -3,21 +3,13 @@ package com.example.albums
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
-import com.example.albums.Fragments.FragmentAlbum
-import com.example.albums.Fragments.FragmentHome
-import com.example.albums.Fragments.FragmentSettings
 import com.example.albums.databinding.ActivityMainBinding
-import com.example.albums.databinding.FragmentSettingsBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,12 +22,11 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-
         val host: NavHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
 
         // Set up Action Bar
-        val navController = host.navController
+        navController = host.navController
 
         setupBottomNavMenu(navController)
     }
@@ -48,5 +39,4 @@ class MainActivity : AppCompatActivity() {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav?.setupWithNavController(navController)
     }
-
 }
